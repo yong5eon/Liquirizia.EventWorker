ae5e79fc49010f30f5a37844804368b62c2073ba
허용선 <yongseon.heo@gmail.com>
2025-03-14 18:47:41 +0900

    샘플 변경

--- sample/Sample.EventBroker.py
+++ sample/Sample.EventBroker.py
@@ -9,6 +9,12 @@ from Liquirizia.EventWorker import (
 	ThreadEventRunnerPool,
 	ProcessEventRunnerPool,
 )
+from Liquirizia.EventWorker import (
+	EventRunner,
+	EventRunnerComplete,
+	EventRunnerError,
+	EventProperties,
+)
 
 from Liquirizia.EventBroker import Helper
 from Liquirizia.EventBroker.Implements.RabbitMQ import (
@@ -19,16 +25,79 @@ from Liquirizia.EventBroker.Implements.RabbitMQ import (
 	EventHandler,
 ) 
 
-from SampleEventRunner import *
-
 from Liquirizia.Logger import (
 	LOG_INIT,
 	LOG_LEVEL_DEBUG,
 	LOG_INFO,
+	LOG_ERROR,
+	LOG_DEBUG,
 )
 
 from signal import signal, SIGINT
 from random import randint
+
+
+class Complete(EventRunnerComplete):
+	def __call__(self, completion):
+		LOG_INFO('Complete : {}'.format(completion))
+		return
+	
+class Error(EventRunnerError):
+	def __call__(self, error: BaseException):
+		LOG_ERROR('Error : {}'.format(str(error)), e=error)
+		return
+
+
+@EventProperties(
+	event='+',
+	completes=Complete(),
+	errors=Error(),
+)
+class Add(EventRunner):
+	def run(self, a: int , b: int):
+		LOG_DEBUG('Run : {} + {}'.format(a, b))
+		return a + b
+
+@EventProperties(
+	event='-',
+	completes=Complete(),
+	errors=Error(),
+)
+class Sub(EventRunner):
+	def run(self, a: int , b: int):
+		LOG_DEBUG('Run : {} - {}'.format(a, b))
+		return a - b
+
+@EventProperties(
+	event='*',
+	completes=Complete(),
+	errors=Error(),
+)
+class Mul(EventRunner):
+	def run(self, a: int , b: int):
+		LOG_DEBUG('Run : {} * {}'.format(a, b))
+		return a * b
+
+@EventProperties(
+	event='/',
+	completes=Complete(),
+	errors=Error(),
+)
+class Div(EventRunner):
+	def run(self, a: int , b: int):
+		LOG_DEBUG('Run : {} / {}'.format(a, b))
+		return a / b
+
+
+@EventProperties(
+	event='%',
+	completes=Complete(),
+	errors=Error(),
+)
+class Mod(EventRunner):
+	def run(self, a: int , b: int):
+		LOG_DEBUG('Run : {} % {}'.format(a, b))
+		return a % b
 
 
 class SampleEventHandler(EventHandler):
--- sample/Sample.py
+++ sample/Sample.py
@@ -9,18 +9,87 @@ from Liquirizia.EventWorker import (
 	ThreadEventRunnerPool,
 	ProcessEventRunnerPool,
 )
-
-from SampleEventRunner import *
+from Liquirizia.EventWorker import (
+	EventRunner,
+	EventRunnerComplete,
+	EventRunnerError,
+	EventProperties,
+)
 
 from Liquirizia.Logger import (
 	LOG_INIT,
 	LOG_LEVEL_DEBUG,
 	LOG_INFO,
+	LOG_ERROR,
+	LOG_DEBUG,
 )
 
 from signal import signal, SIGINT
 from random import randint
 from time import sleep
+
+
+class Complete(EventRunnerComplete):
+	def __call__(self, completion):
+		LOG_INFO('Complete : {}'.format(completion))
+		return
+	
+class Error(EventRunnerError):
+	def __call__(self, error: BaseException):
+		LOG_ERROR('Error : {}'.format(str(error)), e=error)
+		return
+
+
+@EventProperties(
+	event='+',
+	completes=Complete(),
+	errors=Error(),
+)
+class Add(EventRunner):
+	def run(self, a: int , b: int):
+		LOG_DEBUG('Run : {} + {}'.format(a, b))
+		return a + b
+
+@EventProperties(
+	event='-',
+	completes=Complete(),
+	errors=Error(),
+)
+class Sub(EventRunner):
+	def run(self, a: int , b: int):
+		LOG_DEBUG('Run : {} - {}'.format(a, b))
+		return a - b
+
+@EventProperties(
+	event='*',
+	completes=Complete(),
+	errors=Error(),
+)
+class Mul(EventRunner):
+	def run(self, a: int , b: int):
+		LOG_DEBUG('Run : {} * {}'.format(a, b))
+		return a * b
+
+@EventProperties(
+	event='/',
+	completes=Complete(),
+	errors=Error(),
+)
+class Div(EventRunner):
+	def run(self, a: int , b: int):
+		LOG_DEBUG('Run : {} / {}'.format(a, b))
+		return a / b
+
+
+@EventProperties(
+	event='%',
+	completes=Complete(),
+	errors=Error(),
+)
+class Mod(EventRunner):
+	def run(self, a: int , b: int):
+		LOG_DEBUG('Run : {} % {}'.format(a, b))
+		return a % b
 
 
 class SampleEventInvoker(EventInvoker):
